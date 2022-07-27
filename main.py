6a370f5e4eb605caa0599da2640ea596b9e3cffc
joebed <joebed@umich.edu>
2022-07-26 20:48:23 -0400

    fundraiser can be made

--- main.py
+++ main.py
@@ -2,14 +2,27 @@ from csv import reader
 
 from models.fundraiser import Fundraiser
 from models.level import Level
+from xrpl.account import get_account_info
+from xrpl.clients import JsonRpcClient
+testnet_url = "https://s.altnet.rippletest.net:51234"
+client = JsonRpcClient(testnet_url)
 
 addies = []
 with open("addresses.csv", "r") as f:
     rows = reader(f, delimiter='\n')
     for row in rows:
         addies.append(row[0])
-host = addies.pop()
+host = addies.pop(0)
 
 fundraiser = Fundraiser("Sandwich Fund", 20000000, [Level(0, 10000, "Friends"), Level(1, 100000, "Bigbois")], host)
 
 print(fundraiser)
+
+host_response = get_account_info(host, client)
+print(f"Balance: {host_response.result['account_data']['Balance']}")
+
+fundraiser.pledge(addies[0], 2000)
+
+host_response = get_account_info(host, client)
+print(f"Balance: {host_response.result['account_data']['Balance']}")
+
--- models/fundraiser.py
+++ models/fundraiser.py
@@ -1,6 +1,9 @@
 """Fundraiser instance."""
 from models.level import Level
-
+from xrpl.clients import JsonRpcClient
+from xrpl.models.transactions.payment import Payment
+from xrpl.transaction import safe_sign_and_autofill_transaction, send_reliable_submission, XRPLReliableSubmissionException
+from xrpl.utils import drops_to_xrp
 class Fundraiser():
     def __init__(
             self,
@@ -16,8 +19,39 @@ class Fundraiser():
     
     def pledge(
             self,
-            sender: str):
-        pass
+            sender: str,
+            amt: int):
+            
+        testnet_url = "https://s.altnet.rippletest.net:51234"
+        client = JsonRpcClient(testnet_url)
+
+        pledge = Payment(
+                    account=sender,
+                    amount=amt,
+                    destination=self.host,
+                 )
+        signed_tx = safe_sign_and_autofill_transaction(
+            pledge, sender, client)
+
+        tx_id = signed_tx.get_hash()
+
+        try:
+            tx_response = send_reliable_submission(signed_tx, client)
+        except XRPLReliableSubmissionException as e:
+            exit(f"Submit failed: {e}")
+
+        import json
+        print(json.dumps(tx_response.result, indent=4, sort_keys=True))
+        print(f"Explorer link: https://testnet.xrpl.org/transactions/{tx_id}")
+        metadata = tx_response.result.get("meta", {})
+        if metadata.get("TransactionResult"):
+            print("Result code:", metadata["TransactionResult"])
+        if metadata.get("delivered_amount"):
+            print("XRP delivered:", drops_to_xrp(
+                        metadata["delivered_amount"]))
+            self.drops_raised += metadata["delivered_amount"]
+
+        
 
     def __repr__(self) -> str:
         return f"Fundraiser {self.name}\n\thost:\t{self.host}\n\tprogress:\t{self.drops_raised}/{self.goal}"
